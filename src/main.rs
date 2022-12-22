cfaa83e013cbad22199b51b315df786bd51cd88b
Rhelvetican <bionicvnb@gmail.com>
2022-12-22 15:36:21 +0700

    Fixes

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-#![allow(non_snake_case)]
-#![allow(non_camel_case_types)]
-#![allow(dead_code)]
-struct pos {
+fn disable() {
+    #![allow(non_snake_case)]
+    #![allow(non_camel_case_types)]
+    #![allow(dead_code)]
+}
+struct Pos {
     x: i64,
     y: i64
 }
-struct pos3d {
+struct Pos3d {
     x: i64,
     y: i64,
     z: i64
@@ -16,8 +18,8 @@ fn main() {
     let a:i128 = 256;
     println!("{} + 1 = {}", a , add_one(a));
     update_two();
-    let position = pos {x: 32, y: 43};
-    let pos3d = pos3d {x: 64, y: 72, z: 12};
+    let position = Pos {x: 32, y: 43};
+    let pos3d = Pos3d {x: 64, y: 72, z: 12};
     println!("Test position: x = {}, y = {}", position.x, position.y);
     println!("Test 3D position: x = {}, y = {}, z = {}", pos3d.x, pos3d.y, pos3d.z);
 }
