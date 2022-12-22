e4dcc604baff87b329508e88814145910258dfb4
Rhelvetican <bionicvnb@gmail.com>
2022-12-22 14:30:14 +0700

    Changes 2

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 fn main() {
     println!("Hello World!");
-
-    let my_name:&str = "Twelvetican";
-    let my_rating:f32 = 4.7;
-    let is_liking:bool = true;
-    println!("My name is: {}",my_name);
-    println!("My rating is: {}",my_rating);
+    update();
+}
+fn update() {
+    let my_name: &str = "Twelvetican";
+    let my_rating: f32 = 4.7;
+    let is_liking: bool = true;
+    println!("My name is: {}", my_name);
+    println!("My rating is: {}", my_rating);
     println!("I like this: {}", is_liking);
+}
+fn add_one() {
 }
